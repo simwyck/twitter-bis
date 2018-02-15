45b29460797e0923e4697047a1e9ed9e61c280ed
Simon Wyckaert <simon@digitold.com>
2018-02-15 12:35:56 +0100

    push heroku 2

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Twitter Bis
 
-Init readme
-
 Heroku:  https://j29-twitter-bis-swy.herokuapp.com/
 
 ---
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'home#index'
+  root 'tweets#new'
 
   get 'home/index'
 
